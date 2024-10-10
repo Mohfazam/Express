49d037cc32f4a5d39bea88c099eb466628af5fad
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-10 06:14:36 +0530

    Basics of mongodb

--- Exp_app/routes/index.js
+++ Exp_app/routes/index.js
@@ -7,11 +7,21 @@ router.get('/', function(req, res, next) {
   res.render("index");
 });
 
-router.get('/create', async function(req, res, next) {
+router.get('/create1', async function(req, res, next) {
   let x = await usermodel.create({
     username: "mohfazam",
     name: "sarwar",
     age: 20
+
+  });
+  res.send(x);
+});
+
+router.get('/create2', async function(req, res, next) {
+  let x = await usermodel.create({
+    username: "John",
+    name: "John doe",
+    age: 40
 
   });
   res.send(x);
@@ -22,5 +32,16 @@ router.get("/allusers", async function(req, res){
 
  res.send(allusers);
 });
+
+router.get("/delete1", async function(req, res){
+  let deleteduser = await usermodel.findOneAndDelete({age: 40});
+  res.send(deleteduser);
+});
+
+router.get("/delete2", async function(req, res){
+  let deleteduser = await usermodel.findOneAndDelete({age: 20});
+  res.send(deleteduser);
+});
+
 
 module.exports = router;
