d0293c191e16377f9e103f00ad4775cf32cf4df7
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-10 23:44:35 +0530

    Token generation

--- authentication/index.js
+++ authentication/index.js
@@ -4,7 +4,18 @@ app.use(express.json());
 
 const users = [];
 
-app.post("/signup", function(req, res){
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+app.post("/signup", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
@@ -19,7 +30,34 @@ app.post("/signup", function(req, res){
     });
 });
 
-app.post("/signin", function(req, res){
+
+
+app.post("/signin", function (req, res) {
+
+    let founduser = null;
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username == username && users[i].password == password) {
+            founduser = users[i];
+        }
+    }
+
+    if (founduser) {
+        const token = generateToken();
+        founduser.token = token;
+
+        res.json({
+            token: token
+        });
+    }
+    else {
+        res.status(403).send({
+            msg: "Invalid username or password"
+        });
+    }
 
 });
 
