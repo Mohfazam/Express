48e08620ba09f9d6e00b450f55a4c465c06d1f04
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-13 11:36:55 +0530

    jwt token

--- authentication/index.js
+++ authentication/index.js
@@ -1,19 +1,9 @@
 const express = require("express");
+const JWT_SECRET = "randomstringliteral";
 const app = express();
 app.use(express.json());
 
 const users = [];
-
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
 
 app.post("/signup", function (req, res) {
     const username = req.body.username;
