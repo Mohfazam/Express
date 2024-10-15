195d1af2ba571aabe8eefc36bdfc3ec099b1427e
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-16 01:14:52 +0530

    Nodejs JWT based Authentication

---
+++ authentication/Test2/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "Helloworld";
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.post("/signup", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    });
+
+    res.json({
+        msg: "Your signed up"
+    })
+});
+
+app.post("/signin", function(req, res){
+    let founduser = null;
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    founduser = users.find(u => u.username === username && u.password === password);
+
+    if(founduser){
+        const token = jwt.sign({ username: username }, JWT_SECRET);
+
+        res.json({
+            msg:"Your Logged in", 
+            token: token
+        })
+    }
+    else{
+        res.status(403).json({
+            msg: "Invalid Username or password"
+        })
+    }
+});
+
+
+app.get("/me", function(req, res){
+    const token = req.headers.token;
+    const decodedinfo = jwt.verify(token, JWT_SECRET);
+    console.log(decodedinfo);
+    const username = decodedinfo.username;
+
+    const user = users.find(u => u.username === username);
+
+    if(user){
+        res.json({
+            username: user.username,
+            password: user.password
+        });
+    }
+    else{
+        res.status(401).json({
+            msg: "Unauthorized"
+        })
+    }
+});
+
+
+app.listen(3000);
